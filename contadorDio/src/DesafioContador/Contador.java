bb9124acdd7a1300deacd0b135dee99e87360308
Abner Oliveira <111444549+xabner233@users.noreply.github.com>
2023-06-01 10:27:44 -0300

    Loop while, e exceções adicionadas

--- contadorDio/src/DesafioContador/Contador.java
+++ contadorDio/src/DesafioContador/Contador.java
@@ -1,32 +1,50 @@
 package DesafioContador;
 
 import java.util.Scanner;
-
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        boolean repetir = true;
 
-        try {
-            // chamando o método contendo a lógica de contagem
-            contar(parametroUm, parametroDois);
-        } catch (ParametrosInvalidosException exception) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        while (repetir) {
+            System.out.println("Digite o primeiro parâmetro:");
+            int parametroUm;
+            try {
+                parametroUm = Integer.parseInt(terminal.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: O valor digitado não é um número inteiro válido.");
+                continue;
+            }
+
+            System.out.println("Digite o segundo parâmetro:");
+            int parametroDois;
+            try {
+                parametroDois = Integer.parseInt(terminal.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: O valor digitado não é um número inteiro válido.");
+                continue;
+            }
+
+            try {
+                contar(parametroUm, parametroDois);
+            } catch (ParametrosInvalidosException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("Deseja realizar outra contagem? (S/N)");
+            String opcao = terminal.nextLine().toUpperCase();
+            repetir = opcao.equals("S");
         }
 
+        System.out.println("Programa encerrado.");
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametroUm >= parametroDois) {
+        if (parametroDois <= parametroUm) {
             throw new ParametrosInvalidosException();
         }
 
         int contagem = parametroDois - parametroUm;
-        // realizar o for para imprimir os números com base na variável contagem
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
